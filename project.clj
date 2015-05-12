d7902e0f95571e6f72ccdb9b4ac7a238af43f5e0
Sam Saccone <samccone@gmail.com>
2015-05-11 21:52:00 -0400

    :fish: Set uberjar name

--- project.clj
+++ project.clj
@@ -3,11 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:uberjar {:aot :all}}
   :plugins [[lein-ring "0.8.11"]
             [lein-environ "1.0.0"]]
   :min-lein-version "2.5.0"
   :ring {:handler moves-server.core/app}
+  :main ^:skip-aot moves-server.core
+  :uberjar-name "moves-server-standalone.jar"
+  :profiles {:uberjar {:aot :all}}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.2"]
                  [clj-http "1.1.2"]
